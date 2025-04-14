35c8de7dc02cc45b3ab08a6203fa140de72693ad
SoWhatDS <103374600+SoWhatDS@users.noreply.github.com>
2025-04-14 22:52:15 +0300

    Task 1. Написать программу сложения двух чисел.

--- GBChepter1/GBChepter1/Program.cs
+++ GBChepter1/GBChepter1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,53 @@ namespace GBChepter1
     {
         static void Main(string[] args)
         {
+            #region Task1 Написать программу сложения двух чисел.
+
+            int _firstNumber;
+            int _secondNumber;
+            int _sum;
+  
+            Console.Write("Введите первое число: ");
+
+            _firstNumber = CanParse(Console.ReadLine());
+
+            Console.Write("Введите второе число: ");
+
+            _secondNumber = CanParse(Console.ReadLine());
+
+            _sum = Sum(_firstNumber, _secondNumber);
+
+            Console.Write($"Сумма чисел равна: {_sum}");
+            Console.ReadKey();
+            Console.Clear();
+            #endregion
+        }
+
+        static int CanParse(string userInput)
+        {
+            int userInputNumber;
+
+            if (Int32.TryParse(userInput, out int inputNumber))
+            {
+                userInputNumber = inputNumber;
+            }
+            else
+            {
+                Console.Write("Недопустимое значение!!! Значению присвоен ноль");
+                userInputNumber = 0;
+            }
+
+            return userInputNumber;
+        }
+
+        static int Sum(int a,int b)
+        {
+            return a + b;
         }
     }
 }
+
+                 
+
+
+ 
